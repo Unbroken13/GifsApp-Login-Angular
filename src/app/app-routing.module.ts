bc90f645865b7474ac0cf642228e7afd5772af56
Gabriel Orellana <gabi.ale.orellana@gmail.com>
2023-08-07 21:25:55 -0300

    se crean rutas de navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './log/login/login.component';
+import { RegistrarUsuarioComponent } from './log/registrar-usuario/registrar-usuario.component';
+import { VerificarUsuarioComponent } from './log/verificar-usuario/verificar-usuario.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent },
+  {path: 'registrar-usuario', component: RegistrarUsuarioComponent },
+  {path: 'verificar-correo', component: VerificarUsuarioComponent },
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
