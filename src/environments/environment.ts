e6f1f20e8e1118256b80a076fea38b091b94bff0
Gabriel Orellana <gabi.ale.orellana@gmail.com>
2023-08-10 06:13:38 -0300

    se agregaro la direccion del DB a los environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  firebase: {
+    projectId: 'gifsapp-476ac',
+    appId: '1:31013335451:web:4e43bb33b53e8718fff13e',
+    storageBucket: 'gifsapp-476ac.appspot.com',
+    apiKey: 'AIzaSyA0L1jTiiw03j7q9dUzcjt8EHL7vI0LgIE',
+    authDomain: 'gifsapp-476ac.firebaseapp.com',
+    messagingSenderId: '31013335451',
+  },
+  // production: false,
+  //   firebaseConfig: {
+  //       apiKey: "AIzaSyA0L1jTiiw03j7q9dUzcjt8EHL7vI0LgIE",
+  //       authDomain: "gifsapp-476ac.firebaseapp.com",
+  //       projectId: "gifsapp-476ac",
+  //       storageBucket: "gifsapp-476ac.appspot.com",
+  //       messagingSenderId: "31013335451",
+  //       appId: "1:31013335451:web:4e43bb33b53e8718fff13e"
+  //   }
+};
